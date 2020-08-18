3eaf7101ef7c2ec5b1bac83651d02702313e8473
Agata <cze-aga@o2.pl>
2020-08-18 23:25:07 +0200

    module 2

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class QuizQuestionButton extends React.Component {
+  render() {
+    return (
+      <li>
+        <button>{this.props.buttonText}</button>
+      </li>
+    );
+  }
+}
+export default QuizQuestionButton;
