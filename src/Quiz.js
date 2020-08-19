b03fbf2d639c19b6822af349922cf2f3a30638ce
Agata <cze-aga@o2.pl>
2020-08-19 18:34:15 +0200

    module $

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.jss";
 
 class Quiz extends React.Component {
   constructor(props) {
@@ -9,12 +10,20 @@ class Quiz extends React.Component {
     };
   }
   render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_question.length;
     return (
       <div className="QuizQuestion">
-        <QuizQuestion
-          instruction_text={quizData.quiz_questions[0].instruction_text}
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            instruction_text={quizData.quiz_questions[0].instruction_text}
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
